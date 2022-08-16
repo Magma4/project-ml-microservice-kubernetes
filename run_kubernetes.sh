b4abcc7e17ca647454737d3a4da01231c5b7ad87
Magma <raymondamoateng@gmail.com>
2022-08-16 11:11:46 +0100

    Udacity project 4 completion

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="magma4/microserviceproject:v1.0.0"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run microserviceproject \
+    --image=magma4/microserviceproject:v1.0.0 \
+    --port=80 --labels app=microserviceproject
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward microserviceproject 8000:80
